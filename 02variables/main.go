71e5824c092698d876f50a6038fcbe1610de1846
Ronny Ardi <ronnyardianto29@gmail.com>
2022-07-10 00:02:05 +0700

    02variable

---
+++ 02variables/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+const LoginToken string = "ghabbries" // public
+
+func main() {
+	var username string = "hitesh"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float64 = 255.455445469963234
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	// default values and some aliases
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	// implicit type
+	var website = "learncodeonline.in"
+	fmt.Println(website)
+
+	// no var style
+	numberOfUser := 300000.0
+	fmt.Println(numberOfUser)
+
+	// public
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
+}
